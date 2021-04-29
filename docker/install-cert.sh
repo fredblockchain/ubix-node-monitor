ed95b20a700fd0cc6f18ed48af851923576b72d3
fred <fred.c@runbox.com>
2021-04-29 20:23:36 +0200

    Solving certificate file extention

--- docker/install-cert.sh
+++ docker/install-cert.sh
@@ -1,3 +1,3 @@
 wget -t0 -c http://crl.usertrust.com/USERTrustRSACertificationAuthority.crl
-mv ./USERTrustRSACertificationAuthority.crl /usr/local/share/ca-certificates/USERTrustRSACertificationAuthority.crl
+mv ./USERTrustRSACertificationAuthority.crl /usr/local/share/ca-certificates/USERTrustRSACertificationAuthority.crt
 sudo update-ca-certificates
