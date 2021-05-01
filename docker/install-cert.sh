87a713d2a6207987fdeb09c7b50d59bf316f1231
fred <fred.c@runbox.com>
2021-05-01 23:07:57 +0200

    delete unused file

--- docker/install-cert.sh
+++
@@ -1,3 +0,0 @@
-wget -t0 -c http://crl.usertrust.com/USERTrustRSACertificationAuthority.crl
-mv ./USERTrustRSACertificationAuthority.crl /usr/local/share/ca-certificates/USERTrustRSACertificationAuthority.crt
-sudo update-ca-certificates
